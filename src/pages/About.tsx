db28d67e1fb8df980930b1c861dc39ff19603dbe
ahmed <ahmosakic@gmail.com>
2023-09-05 15:27:20 +0200

    added about text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+ <h1>About</h1>
+
+ <p>This application is free to use and is created for the purpose of helping users pass their AWS exam.</p>
+ <p>Questions on this application are similar to those appearing on real exam.</p>
+ <p>They are divided in exam sets, each on different difficulty.</p>
+
+ <h3>Best of luck on your exam.</h3>
+    </div>
   )
 }
